2f47fe4db2475c02793e18527a2ae433fed2a1c0
Victoria Medina <vicmedina@aude.edu.ar>
2023-11-28 09:37:41 -0300

    eliminamos archivos y codigo que no se usan

--- demo/src/main/java/com/example/demo/controller/DuenioRepository.java
+++ demo/src/main/java/com/example/demo/controller/DuenioRepository.java
@@ -9,35 +9,4 @@ import com.example.demo.entity.Duenio;
 
 public interface DuenioRepository extends JpaRepository<Duenio, Integer> {
 
-    public default List<String> listarSegunEdifico(Integer codigoedificio) {
-        List<Duenio> duenios = findAll();
-        List<String> resultado = new ArrayList<String>();
-        for (Duenio duenio : duenios) {
-            if (duenio.getUnidad().getEdificio().getCodigo() == codigoedificio) {
-                resultado.add(duenio.toString());
-            }
-        }
-        return resultado;
-    }
-
-    public default List<String> listarTodos() {
-        List<Duenio> duenios = findAll();
-        List<String> resultado = new ArrayList<String>();
-        for (Duenio duenio : duenios) {
-            resultado.add(duenio.toString());
-        }
-        return resultado;
-    }
-
-    public default Duenio findDuenioByDocumento(String string) {
-        List<Duenio> duenios = findAll();
-        Duenio duenioExistente = null;
-        for (Duenio duenio : duenios) {
-            if (duenio.getPersona().getDocumento().equals(string)) {
-                System.out.println("Duenio encontrado: " + duenio.toString());
-                duenioExistente = duenio;
-            }
-        }
-        return duenioExistente;
-    }
 }
--- demo/src/main/java/com/example/demo/controller/EstadoRepository.java
+++ demo/src/main/java/com/example/demo/controller/EstadoRepository.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Estado;
