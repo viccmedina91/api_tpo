a94a0a5555cc3e55d9e7163b45babb9c5471f0ce
Victoria Medina <vicmedina@aude.edu.ar>
2023-12-02 11:24:02 -0300

    estilos de error para buscar dueño segun id unidad

--- api_front/src/SearchDuenioPorUnidad.js
+++ api_front/src/SearchDuenioPorUnidad.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import ShowList from "./ShowList";
 import FormSearch from './Forms/FormSearch';
+import Error from './Error';
 
 function SearchDuenioPorUnidad() {
     const [responseData, setResponseData] = useState(null);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(false);
 
     const handleSubmit = (campo) => {
         // Devuelve la unidad según el identificador ingresado
@@ -17,7 +18,9 @@ function SearchDuenioPorUnidad() {
             })
             .then((data) => {
                 setResponseData(data);
-                console.log(typeof (responseData));
+                if (data.mensaje.toLowerCase().includes('error')) {
+                    setError(data.mensaje);
+                }
             })
             .catch((error) => {
                 setError(error.mensaje);
@@ -27,11 +30,21 @@ function SearchDuenioPorUnidad() {
 
     return (
         <div>
-            <h2>Buscar Dueño según Identificador de Unidad</h2>
-            <FormSearch onSubmit={handleSubmit} />
-            {responseData && (<ShowList result={JSON.stringify(responseData, null, 2)} />
-            )}
-            {error && <p className="text-danger mt-3">{error}</p>}
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <h2>Buscar Dueño según Identificador de Unidad</h2>
+                        <FormSearch onSubmit={handleSubmit} />
+                        {responseData && (
+                            <div>
+                                {error ? (
+                                    <Error message={error} />
+                                ) : <ShowList result={JSON.stringify(responseData, null, 2)} />}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
