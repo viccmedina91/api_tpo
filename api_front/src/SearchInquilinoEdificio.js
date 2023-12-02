c611cef57a6065c16995563aa62fa25ca91f1362
Victoria Medina <vicmedina@aude.edu.ar>
2023-12-02 15:47:04 -0300

    estilos de msj de error para inquilinos por edificio

--- api_front/src/SearchInquilinoEdificio.js
+++ api_front/src/SearchInquilinoEdificio.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import ShowList from "./ShowList";
 import FormSearch from './Forms/FormSearch';
+import Error from './Error';
 
 function SearchInquilinoEdificio() {
     const [responseData, setResponseData] = useState(null);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(false);
 
     const handleSubmit = (campo) => {
         // // Según el código de Edificio ingresado, devuelve todos los Inquilinos
@@ -18,7 +19,9 @@ function SearchInquilinoEdificio() {
             .then((data) => {
                 // Manejar la respuesta del backend, esto no se que hace, preguntar
                 setResponseData(data);
-                console.log(typeof (responseData));
+                if (data.mensaje.toLowerCase().includes('error')) {
+                    setError(data.mensaje);
+                }
             })
             .catch((error) => {
                 console.error('Error al hacer la solicitud:', error);
@@ -28,11 +31,21 @@ function SearchInquilinoEdificio() {
 
     return (
         <div>
-            <h2>Listar Inquilinos según Edificio</h2>
-            <FormSearch onSubmit={handleSubmit} />
-            {responseData && (<ShowList result={JSON.stringify(responseData, null, 2)} />
-            )}
-            {error && <p className="text-danger mt-3">{error}</p>}
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <h2>Listar Inquilinos según Edificio</h2>
+                        <FormSearch onSubmit={handleSubmit} />
+                        {responseData && (
+                            <div>
+                                {error ? (
+                                    <Error message={error} />
+                                ) : <ShowList result={JSON.stringify(responseData, null, 2)} />}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
