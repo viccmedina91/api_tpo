82178907f26d15d2384760855bba7ab5eeb65562
Victoria Medina <vicmedina@aude.edu.ar>
2023-12-03 00:22:56 -0300

    agregamos la tabla que muestra las unidades

--- api_front/src/Tablas/TablaUnidades.js
+++ api_front/src/Tablas/TablaUnidades.js
@@ -1,6 +1,7 @@
 const TablaUnidades = ({ result }) => {
-    const unidades = JSON.parse(result);
-
+    const respuesta = JSON.parse(result);
+    const unidades = Array.isArray(respuesta) ? respuesta : [respuesta];
+    console.log(unidades);
     if (!unidades || unidades.length === 0) {
         return <p className="text-muted">No hay unidades para mostrar.</p>;
     }
@@ -10,12 +11,11 @@ const TablaUnidades = ({ result }) => {
             < table className="table table-bordered table-hover" >
                 <thead className="thead-dark">
                     <tr>
-
                         <th>Edificio</th>
                         <th>Código Unidad</th>
                         <th>Número</th>
                         <th>Piso</th>
-
+                        <th>Habitado</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -25,6 +25,7 @@ const TablaUnidades = ({ result }) => {
                             <td>{unidad.identificador}</td>
                             <td>{unidad.numero}</td>
                             <td>{unidad.piso}</td>
+                            <td>{unidad.habitado}</td>
                         </tr>
                     ))}
                 </tbody>
