a7286532002587ea775c1faa12207e3505b38fcd
Victoria Medina <vicmedina@aude.edu.ar>
2023-12-04 01:08:37 -0300

    mejoras en el panel de usuario

--- api_front/src/PanelUsuario.js
+++ api_front/src/PanelUsuario.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Subseccion from './Subseccion';
-import { Dropdown } from 'react-bootstrap';
 import Logout from './Logout';
 
 const PanelUsuario = () => (
     <div className="container mt-5">
         <Logout />
-
-        <div className="row">
-            {/* Tarjetas superiores */}
-
-            <div className="col-md-4">
-                <Dropdown>
-                    <Dropdown.Toggle variant="warning" id="dropdown-basic">
-                        Gestión de Unidades
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                        <Subseccion title="Mis Unidades" to="/unidad/mis" />
-                    </Dropdown.Menu>
-                </Dropdown>
+        <div class="card mb-3">
+            <img src={process.env.PUBLIC_URL + '/img/gunidades.jpg'} className="card-img-top" alt="Gestión de Edificio" style={{ height: '200px' }} />
+            <div class="card-body">
+                <h5 class="card-title">Gestión de Unidades</h5>
+                <p class="card-text">En esta sección encontrarás todo lo necesario para administrar mis unidades</p>
+                <ul>
+                    <li> <Subseccion title="Mis Unidades" to="/unidad/mis" /></li>
+                </ul>
             </div>
-            <div className="col-md-4">
-                <Dropdown>
-                    <Dropdown.Toggle variant="danger" id="dropdown-basic">
-                        Gestión de Reclamos
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Subseccion title="Listar Mis Reclamos" to="/reclamos/mis" />
-                        <Subseccion title="Crear Reclamos" to="/reclamos/crear" />
-                        <Subseccion title="Agregar Imagen a Reclamos" to="/reclamos/agregar/imagen" />
-                        <Subseccion title="Cambiar de estado a Reclamo" to="/reclamos/actualizar/estado" />
-                    </Dropdown.Menu>
-                </Dropdown>
+        </div>
+        <div class="card mb-3">
+            <img src={process.env.PUBLIC_URL + '/img/greclamos.jpg'} className="card-img-top" alt="Gestión de Edificio" style={{ height: '200px' }} />
+            <div class="card-body">
+                <h5 class="card-title">Gestión de Unidades</h5>
+                <p class="card-text">En esta sección encontrarás todo lo necesario para administrar mis unidades</p>
+                <ul>
+                    <li> <Subseccion title="Listar Mis Reclamos" to="/reclamos/mis" /></li>
+                    <hr></hr>
+                    <li><Subseccion title="Crear Reclamos" to="/reclamos/crear" /></li>
+                    <hr></hr>
+                    <li><Subseccion title="Agregar Imagen a Reclamos" to="/reclamos/agregar/imagen" /></li>
+                    <hr></hr>
+                    <li><Subseccion title="Cambiar de estado a Reclamo" to="/reclamos/actualizar/estado" /></li>
+                </ul>
             </div>
         </div>
     </div >
